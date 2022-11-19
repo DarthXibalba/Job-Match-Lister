534d75ecf801a35e3fa47dae7fee6da55a598dd6
Javier Urquizu <javierurquizu@gmail.com>
2022-11-18 22:59:40 -0800

    Added more basic functions to html pkg

--- html/html.go
+++ html/html.go
@@ -3,7 +3,22 @@ package html
 import (
 	"io"
 	"net/http"
+	"net/url"
+	"os"
 )
+
+/**
+ * Get the base url from an http.Response
+ * Returns: baseUrl as a string
+ */
+func GetBaseUrlFromResp(resp *http.Response) string {
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	return baseUrl.String() // := "https://domain.com"
+}
 
 /**
  * Sends a GET request to the given webPageUrl parameter
@@ -21,4 +36,16 @@ func GetHtmlPage(webPageUrl string) (string, error) {
 		return "", err
 	}
 	return string(body), nil
+}
+
+/**
+ * Reads HTML from a file
+ * Returns: The HTML file as a string
+ */
+func ReadHtmlFromFile(fileName string) (string, error) {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
 }
